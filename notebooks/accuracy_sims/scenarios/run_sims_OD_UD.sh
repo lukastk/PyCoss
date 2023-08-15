5d7615ead7595721f77baaece143003c5b42188b
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2023-08-16 00:37:21 +0100

    initial commit

---
+++ notebooks/accuracy_sims/scenarios/run_sims_OD_UD.sh
@@ -0,0 +1,13 @@
+#screen -d -m nice --19 python compute_autocorrelation_fixed_kp.py args long_Nm_fixed_kp_low_temp2 0 10 7 200 0.1 100
+
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 OD_rod_closed__random_force_X.py
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 UD_rod_closed__random_force_X.py
+
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 OD_rod_closed__random_force_X.py
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 UD_rod_closed__random_force_X.py
+
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 OD_rod_closed__random_force_X.py
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 UD_rod_closed__random_force_X.py
+
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 OD_rod_closed__random_force_X.py
+screen -d -m /store/SOFT/ltk26/anaconda3/envs/cosserat/bin/python3.7 UD_rod_closed__random_force_X.py
